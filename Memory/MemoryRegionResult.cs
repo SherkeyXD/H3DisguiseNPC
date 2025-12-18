d4765c1b4d4c51612c621bb2031581c5f3220219
SherkeyXD <57581480+SherkeyXD@users.noreply.github.com>
2025-12-18 14:26:50 +0800

    chore: format files

--- Memory/MemoryRegionResult.cs
+++ Memory/MemoryRegionResult.cs
@@ -2,9 +2,9 @@ namespace Memory;
 
 internal struct MemoryRegionResult
 {
-	public UIntPtr CurrentBaseAddress { get; set; }
+    public UIntPtr CurrentBaseAddress { get; set; }
 
-	public long RegionSize { get; set; }
+    public long RegionSize { get; set; }
 
-	public UIntPtr RegionBase { get; set; }
+    public UIntPtr RegionBase { get; set; }
 }
--- Utils.cs
+++ Utils.cs
@@ -2,23 +2,23 @@ using System.Diagnostics;
 
 public static class Utils
 {
-	public static string ToLiteralByteArrayString(this byte[] bytes)
-	{
-		string text = "";
-		foreach (byte b in bytes)
-		{
-			text = text + b.ToString("X2") + " ";
-		}
-		return text.TrimEnd(' ');
-	}
+    public static string ToLiteralByteArrayString(this byte[] bytes)
+    {
+        string text = "";
+        foreach (byte b in bytes)
+        {
+            text = text + b.ToString("X2") + " ";
+        }
+        return text.TrimEnd(' ');
+    }
 
-	public static Process GetProcess(string processName)
-	{
-		return Process.GetProcessesByName(processName)[0];
-	}
+    public static Process GetProcess(string processName)
+    {
+        return Process.GetProcessesByName(processName)[0];
+    }
 
-	public static bool ProcessOpen(string processName)
-	{
-		return Process.GetProcessesByName(processName).Length != 0;
-	}
+    public static bool ProcessOpen(string processName)
+    {
+        return Process.GetProcessesByName(processName).Length != 0;
+    }
 }
